e9445c1f46a76cdc7366dbab6261976c2b17232e
Vytek <enrico.speranza@gmail.com>
2017-07-21 10:44:45 +0200

    First Commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+using System.Net;
+using Hazel;
+using Hazel.Udp;
+
+/// <summary>
+/// Network manager.
+/// </summary>
+public class NetworkManager : MonoBehaviour {
+
+	public int portNumber = 4296;
+	public string ipAddress = "127.0.0.1";
+
+	private static NetworkManager _instance = null;
+
+	// Client Data
+	Connection serverConn;
+
+	/// <summary>
+	/// Gets the instance.
+	/// </summary>
+	/// <value>The instance.</value>
+	public static NetworkManager instance {
+		get {
+			if (_instance == null) {
+				_instance = FindObjectOfType<NetworkManager> ();
+				if (_instance == null) {
+					GameObject go = new GameObject ();
+					go.name = "NetworkManager";
+					instance = go.AddComponent<NetworkManager> ();
+					DontDestroyOnLoad (go);
+				}
+				Debug.Assert (_instance != null);
+			}
+			return _instance;
+		}
+		set {
+		}
+	}
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake()
+	{
+		if(_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start() {
+		Debug.Log("Network idle.");
+	}
+
+	void OnDestroy() {
+		if (serverConn != null) serverConn.Close();
+	}
+
+	#region Client
+	public void StartClient(string ipAddress) {
+		NetworkEndPoint endPoint = new NetworkEndPoint(ipAddress, portNumber);
+		serverConn = new UdpClientConnection(endPoint);
+		serverConn.DataReceived += HandleDataFromServer;
+		Debug.Log("Connecting to " + endPoint);
+		serverConn.Connect();
+	}
+
+	void HandleDataFromServer(object sender, DataReceivedEventArgs args) {
+		Connection connection = (Connection)sender;
+		Debug.Log("Received " + args.Bytes.Length + " bytes from server at " + connection.EndPoint.ToString());
+		//connection.SendBytes(args.Bytes, args.SendOption);      
+		args.Recycle();
+	}
+
+	void HandleServerDisconnect(object sender, DisconnectedEventArgs args) {
+		Connection connection = (Connection)sender;
+		Debug.Log("Server connection at " + connection.EndPoint + " lost");
+		serverConn = null;
+		args.Recycle();
+	}
+	#endregion
+}
